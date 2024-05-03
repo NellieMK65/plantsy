6f407ba24238b025c3ce17898edba0fa14591fe7
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-05-03 09:13:10 +0300

    wip: create add plant form

--- src/components/AddPlant.jsx
+++ src/components/AddPlant.jsx
@@ -1,5 +1,22 @@
 function AddPlant() {
-	return <div>AddPlant</div>;
+	return (
+		<div className="bg-green-200 p-10 my-5">
+			<h4 className="font-bold text-3xl">New Plant</h4>
+			<form className="w-full flex gap-x-5">
+				<input
+					type="text"
+					placeholder="Plant name"
+					className="border-b-2 border-gray-600 outline-none focus:border-green-500 w-full"
+				/>
+
+				<input
+					type="text"
+					placeholder="image.jpg"
+					className="border-b-2 border-gray-600 outline-none focus:border-green-500 w-full"
+				/>
+			</form>
+		</div>
+	);
 }
 
 export default AddPlant;
