09626857a2df5409dd407a95c6de037117c44036
Nelson Muriithi <nelsonmuriithi65@gmail.com>
2024-05-03 10:13:57 +0300

    setup navigation

---
+++ src/routes/About.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+
+function About() {
+	return (
+		<div>
+			<Navbar />
+			<div className="flex justify-center items-center w-full h-svh">
+				<h1 className="font-bold text-5xl">About us page</h1>
+			</div>
+		</div>
+	);
+}
+
+export default About;
